0b90cff7800ebe667f77bd0ca4ba9cea4ca662d7
shatilsarower <shatild12@gmail.com>
2021-04-20 14:51:15 +0600

    4th

--- lib/Movie/movie_list.dart
+++ lib/Movie/movie_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../movie_list.dart';
+
 class MovieList extends StatefulWidget {
   @override
   _MovieListState createState() => _MovieListState();
@@ -12,6 +14,21 @@ class _MovieListState extends State<MovieList> {
       appBar: AppBar(
         centerTitle: true,
         title: Text("Movie"),
+      ),
+      body: ListView(
+        children: movies
+            .map((movie) => ListTile(
+                  title: Text(movie.name),
+                  subtitle: Text(movie.category),
+                  trailing: Text('${movie.rating}'),
+                  leading: Image.asset(
+                    movie.image,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ))
+            .toList(),
       ),
     );
   }
